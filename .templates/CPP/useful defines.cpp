3438dadbe6d60ac0919eb206079cda7e5408e095
bryan <bryan.deng002@gmail.com>
2022-01-01 10:49:25 -0500

    remove defines

--- .templates/CPP/useful defines.cpp
+++
@@ -1,9 +0,0 @@
-// memset array with val
-#define ms(arr, val) memset(arr, val, sizeof(arr))
-
-// see variable name + value
-// useful for debugging
-#define watch(x) cout << (#x) << ": " << (x) << '\n'
-
-// when you're too lazy to type 1 extra character
-#define nl '\n'
