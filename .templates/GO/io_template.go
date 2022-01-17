56b8e5f8a5df31b49928a0802465554dbe520614
bryan <bryan.deng002@gmail.com>
2022-01-16 20:15:57 -0500

    go template

---
+++ .templates/GO/io_template.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"io/ioutil"
+	"os"
+	"strconv"
+	"strings"
+)
+
+var _tk []string
+var _iop int = 0
+
+func su() int {
+	r, _ := strconv.Atoi(_tk[_iop])
+	_iop++
+	return r
+}
+func main() {
+	bytes, _ := ioutil.ReadAll(os.Stdin)
+	_tk = strings.Fields(string(bytes))
+
+}
